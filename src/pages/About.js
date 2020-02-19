ddd94cc1e39d25024a67cbdb8267d2daa9784691
syakovenko <serglife777@gmail.com>
2020-02-19 16:13:13 +0200

    start

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const About = () => {
+    return(
+        <div className="jumbotron">
+            <h1 className="display-4">Информация обо мне.</h1>
+            <p className="lead">Version application 1.0.0</p>
+        </div>
+    )
+};
